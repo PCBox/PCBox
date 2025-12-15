ab3147bb8c97318ed46c242af82b9ed270de8914
fuel-pcbox <mszoopers@protonmail.com>
2025-12-15 12:00:05 -0600

    HW CURSOR WORKS

--- src/video/vid_riva128.c
+++ src/video/vid_riva128.c
@@ -2701,24 +2701,26 @@ riva128_hwcursor_draw(svga_t *svga, int displine)
     int transparent = 0;
 
 	if(!riva128->cursor_vram) cursor_offset <<= 4;
-    for(int x = 0; x < 32; x++)
-    {
-		cursor_offset += (svga->hwcursor_latch.y - svga->hwcursor_latch.yoff) * 64;
-        uint16_t raw = 0;
-		if(!riva128->cursor_vram) raw = riva128_ramin_read_w(cursor_offset, riva128);
-		else
-		{
-			uint16_t* vram_w = (uint16_t*)svga->vram;
-			raw = vram_w[cursor_offset & 0x3fffff];
-		}
-        replace_bit = raw & 0x8000;
-        transparent = raw == 0;
-        cursor_bitmap = video_15to32[raw & 0x7fff];
-        cursor_offset += 2;
-        uint32_t current_col = buffer32->line[displine][offset + svga->x_add];
-        if(replace_bit) buffer32->line[displine][offset + svga->x_add] = cursor_bitmap | 0xff000000;
-        else buffer32->line[displine][offset + svga->x_add] = transparent ? current_col | 0xff000000 : (current_col ^ cursor_bitmap) | 0xff000000;
-    }
+    for(int y = 0; y < 32; y++)
+	{
+    	for(int x = 0; x < 32; x++)
+    	{
+        	uint16_t raw = 0;
+			if(!riva128->cursor_vram) raw = riva128_ramin_read_w(cursor_offset, riva128);
+			else
+			{
+				uint16_t* vram_w = (uint16_t*)svga->vram;
+				raw = vram_w[cursor_offset & 0x3fffff];
+			}
+        	replace_bit = raw & 0x8000;
+        	transparent = raw == 0;
+        	cursor_bitmap = video_15to32[raw & 0x7fff];
+        	cursor_offset += 2;
+        	uint32_t current_col = buffer32->line[displine][offset + x + svga->x_add];
+        	if(replace_bit) buffer32->line[svga->hwcursor_latch.y + y][offset + x + svga->x_add] = cursor_bitmap | 0xff000000;
+        	else buffer32->line[svga->hwcursor_latch.y + y][offset + x + svga->x_add] = transparent ? current_col | 0xff000000 : (current_col ^ cursor_bitmap) | 0xff000000;
+    	}
+	}
 }
 
 
