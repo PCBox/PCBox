941a2ec4f582f2f7f2e4975ccb1dfbdd5c498646
fuel-pcbox <mszoopers@protonmail.com>
2025-12-15 11:45:31 -0600

    Ugh

--- src/video/vid_riva128.c
+++ src/video/vid_riva128.c
@@ -2689,7 +2689,7 @@ riva128_hwcursor_draw(svga_t *svga, int displine)
     riva128_t *riva128 = (riva128_t *) svga->priv;
     uint16_t startx = riva128->pramdac.cursor_pos & 0xfff;
     uint16_t starty = (riva128->pramdac.cursor_pos >> 16) & 0xfff;
-	uint32_t cursor_offset = riva128->cursor_offset;
+	uint32_t cursor_offset = 0x6000;//riva128->cursor_offset;
 	int         offset = svga->hwcursor_latch.x - svga->hwcursor_latch.xoff;
 
 	pclog("RIVA 128 CURSOR DRAW cursor_offset %08x x %d y %d vram %d\n", riva128->cursor_offset, startx, starty, riva128->cursor_vram);
