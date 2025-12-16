9634cf408ad35fef87c13e6c9bba79063f962fe6
fuel-pcbox <mszoopers@protonmail.com>
2025-12-15 23:44:28 -0600

    moar impls

--- src/video/vid_riva128.c
+++ src/video/vid_riva128.c
@@ -2215,14 +2215,14 @@ riva128_pgraph_execute_command(uint16_t method, uint32_t param, uint32_t ctx,
 
 			if (target)
 			{
-				//pclog("[RIVA 128] PCI notifier at %08x\n", paged_addr);
+				pclog("[RIVA 128] PCI M2MF at %08x\n", paged_addr + riva128->pgraph.m2mf_out_dma_cur);
 				for(int scan = 0; scan < riva128->pgraph.m2mf_scan_num; scan++)
 				{
 					for(uint32_t pixel = riva128->pgraph.m2mf_in_dma; pixel < riva128->pgraph.m2mf_in_dma + riva128->pgraph.m2mf_scan_len; pixel += inc_in)
 					{
 						uint8_t buf = 0;
-						dma_bm_read(paged_addr + riva128->pgraph.m2mf_in_dma + pixel, (uint8_t*)buf, 1, 1);
-						dma_bm_write(paged_addr + riva128->pgraph.m2mf_out_dma, (uint8_t*)buf, 1, 1);
+						dma_bm_read(paged_addr + riva128->pgraph.m2mf_in_dma_cur + pixel, (uint8_t*)buf, 1, 1);
+						dma_bm_write(paged_addr + riva128->pgraph.m2mf_out_dma_cur, (uint8_t*)buf, 1, 1);
 						riva128->pgraph.m2mf_out_dma_cur += inc_out;
 					}
 					riva128->pgraph.m2mf_in_dma_cur += riva128->pgraph.m2mf_pitch_in;
@@ -2236,8 +2236,8 @@ riva128_pgraph_execute_command(uint16_t method, uint32_t param, uint32_t ctx,
 					for(uint32_t pixel = riva128->pgraph.m2mf_in_dma; pixel < riva128->pgraph.m2mf_in_dma + riva128->pgraph.m2mf_scan_len; pixel += inc_in)
 					{
 						uint8_t buf = 0;
-						svga->vram[riva128->pgraph.m2mf_out_dma] = svga->vram[riva128->pgraph.m2mf_in_dma + pixel];
-						svga->changedvram[riva128->pgraph.m2mf_out_dma >> 12] = changeframecount;
+						svga->vram[riva128->pgraph.m2mf_out_dma_cur] = svga->vram[riva128->pgraph.m2mf_in_dma_cur + pixel];
+						svga->changedvram[riva128->pgraph.m2mf_out_dma_cur >> 12] = changeframecount;
 						riva128->pgraph.m2mf_out_dma_cur += inc_out;
 					}
 					riva128->pgraph.m2mf_in_dma_cur += riva128->pgraph.m2mf_pitch_in;
