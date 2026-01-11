fb1f5fd281fc590e11cb3aa0fa65374c321787d6
fuel-pcbox <mszoopers@protonmail.com>
2026-01-10 19:53:35 -0600

    try to fix some boogs

--- src/86box.c
+++ src/86box.c
@@ -1962,7 +1962,7 @@ pc_run(void)
 
     /* Run a block of code. */
     startblit();
-    cpu_exec((int32_t) cpu_s->rspeed / (force_10ms ? 100 : 1000));
+    cpu_exec((int64_t) cpu_s->rspeed / (force_10ms ? 100 : 1000));
     ack_pause();
 #ifdef USE_GDBSTUB /* avoid a KBC FIFO overflow when CPU emulation is stalled */
     if (gdbstub_step == GDBSTUB_EXEC) {
