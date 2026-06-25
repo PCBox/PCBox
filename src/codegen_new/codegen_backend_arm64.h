39571a51c65677a5047b425a35fc3a3a54a901aa
bachimusprime <mszoopers@protonmail.com>
2026-06-25 14:11:02 -0500

    more ndr tuning

--- src/codegen_new/codegen_backend_arm64.h
+++ src/codegen_new/codegen_backend_arm64.h
@@ -1,7 +1,7 @@
 #include "codegen_backend_arm64_defs.h"
 
-#define BLOCK_SIZE  0x8000
-#define BLOCK_MASK  0x7fff
+#define BLOCK_SIZE  0x20000
+#define BLOCK_MASK  0x3ffff
 #define BLOCK_START 0
 
 #define HASH_SIZE   0x20000
