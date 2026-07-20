6b0a7470bd0e3e2ab7226c23a281274509cbd1b2
bachimusprime <mszoopers@protonmail.com>
2026-07-20 14:15:30 -0500

    fix build

--- src/codegen_new/codegen_backend_x86-64_defs.h
+++ src/codegen_new/codegen_backend_x86-64_defs.h
@@ -65,20 +65,3 @@ extern void *codegen_mem_store_double;
 
 extern void *codegen_gpf_rout;
 extern void *codegen_exit_rout;
-
-#define CODEGEN_HOST_CPU_FEATURE_SSE3   (1ULL << 0)
-#define CODEGEN_HOST_CPU_FEATURE_SSSE3  (1ULL << 1)
-#define CODEGEN_HOST_CPU_FEATURE_SSE4_1 (1ULL << 2)
-#define CODEGEN_HOST_CPU_FEATURE_SSE4_2 (1ULL << 3)
-#define CODEGEN_HOST_CPU_FEATURE_AVX    (1ULL << 4)
-#define CODEGEN_HOST_CPU_FEATURE_AVX2   (1ULL << 5)
-#define CODEGEN_HOST_CPU_FEATURE_BMI1   (1ULL << 6)
-#define CODEGEN_HOST_CPU_FEATURE_BMI2   (1ULL << 7)
-
-extern uint64_t codegen_host_cpu_features;
-
-static inline int
-codegen_host_cpu_has_feature(uint64_t feature)
-{
-    return (codegen_host_cpu_features & feature) == feature;
-}
